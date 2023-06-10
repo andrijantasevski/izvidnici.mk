184034d9858a64e4f01a3cee5a28d2a305a6f139
Boban Senic <77991507+Panter530xd@users.noreply.github.com>
2023-06-10 11:15:37 +0200

    Finish animation cards desktop and mobile

--- src/components/CardAnimation.tsx
+++ src/components/CardAnimation.tsx
@@ -7,6 +7,7 @@ type CardData = {
   frontTitle: string;
   frontText: string;
   backImgSrc: string;
+  backTitle: string;
   backText: string;
 };
 
@@ -17,8 +18,10 @@ const cards: CardData[] = [
     frontTitle: "ШТО",
     frontText:
       "Извидничкото движење е доброволно, неполитичко, образовно движење за деца и млади, отворено за сите без разлика на пол, потекло, раса или вера, во согласност со целта, принципите и методот замислени од основачот (Баден Пауел) .",
-    backImgSrc: "path/to/backImage1.jpg",
-    backText: "Back Card 1",
+    backImgSrc: "img/cards/front-1.png",
+    backTitle: "ШТО",
+    backText:
+      "Мисијата на извидништвото е да придонесе во образованието на младите, преку систем на вредности заснован на извидничкиот завет и закони, да помогне во изградба на подобар свет во кој луѓето се остварени како поединци и имаат конструктивна улога во општеството.",
   },
   {
     id: 2,
@@ -26,8 +29,10 @@ const cards: CardData[] = [
     frontTitle: "КАКО",
     frontText:
       "Преку систем на прогресивно само-образование, со интеракција на елементите од извидничкиот метод. Извидничкиот метод има осум составни елементи, подеднакво важни во образованието на децата и младите, а претставуваат клучен пристап и специфичност за извидништвото наспрема останатите образовни движења / организации.",
-    backImgSrc: "path/to/backImage2.jpg",
-    backText: "Back Card 2",
+    backImgSrc: "img/cards/back-2.png",
+    backTitle: "КАКО",
+    backText:
+      "Преку систем на прогресивно само-образование, со интеракција на елементите од извидничкиот метод. Извидничкиот метод има осум составни елементи, подеднакво важни во образованието на децата и младите, а претставуваат клучен пристап и специфичност за извидништвото наспрема останатите образовни движења / организации.",
   },
   {
     id: 3,
@@ -35,8 +40,10 @@ const cards: CardData[] = [
     frontTitle: "ЗОШТО",
     frontText:
       "Целта на извидничкото движење е да придонесе во развојот на младите во постигнување на нивниот целосен физички, интелектуален, емоционален, социјален и духовен потенцијал како поединци, како одговорни граѓани и како членови на нивнителокални, национални и интернационални заедници. ",
-    backImgSrc: "path/to/backImage3.jpg",
-    backText: "Back Card 3",
+    backImgSrc: "img/cards/front-3.png",
+    backTitle: "ЗОШТО",
+    backText:
+      " Извидничката програма која ја уживаат децата, младите и возрасните волонтери се заснова на три широки принципи кои ги претставуваат основните верувања на движењето.Обврска кон себе - Развој на личниот целосен потенцијал, односно достигнување на потполните физички, интелектуални, општествени и духовни можности како поединец",
   },
 ];
 
@@ -45,6 +52,7 @@ const Card: React.FC<CardData> = ({
   frontTitle,
   frontText,
   backImgSrc,
+  backTitle,
   backText,
 }) => {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -55,9 +63,12 @@ const Card: React.FC<CardData> = ({
 
   return (
     <>
-      <div className="relative mx-4 my-4 h-96 w-80" onClick={handleCardClick}>
+      <div
+        className="mx-4 my-4 flex w-full flex-col items-center justify-start md:w-80"
+        onClick={handleCardClick}
+      >
         <motion.div
-          className={` relative flex flex-col items-center justify-start rounded-[17px] bg-primary pb-4 text-center shadow-lg ${
+          className={`relative flex flex-col items-center justify-start rounded-[17px] bg-primary pb-4 text-center shadow-lg ${
             isFlipped ? "hidden" : ""
           }`}
           initial={{ rotateY: 0 }}
@@ -72,22 +83,22 @@ const Card: React.FC<CardData> = ({
           <motion.div className="z-50 px-3 py-10">
             <h2 className="text-2xl font-bold text-white">{frontTitle}</h2>
             <hr className="mx-auto my-2 h-[2px] w-2/6 border-0 bg-secondary" />
-            <h5 className=" h-64 overflow-hidden text-base text-white">
+            <h5 className="h-64 overflow-hidden text-base text-white">
               {frontText}
-              <hr className="mx-auto my-2 h-[2px] w-2/6 border-0 bg-secondary" />
             </h5>
+            <hr className="mx-auto my-2 h-[2px] w-2/6 border-0 bg-secondary" />
           </motion.div>
-          <motion.div className=" absolute bottom-0 left-0">
+          <motion.div className="absolute bottom-0 left-0">
             <motion.img
               src="./img/cards/svg/svg-cards.svg"
               alt={frontTitle}
-              className=" z-0 h-44 w-full object-cover"
+              className="z-0 h-44 w-full object-cover"
             />
           </motion.div>
         </motion.div>
 
         <motion.div
-          className={`card-back absolute flex flex-col items-center justify-center rounded-lg bg-white p-2 text-center shadow-lg ${
+          className={`relative flex flex-col items-center justify-start rounded-[17px] bg-primary pb-4 text-center shadow-lg ${
             isFlipped ? "" : "hidden"
           }`}
           initial={{ rotateY: 180 }}
@@ -97,9 +108,23 @@ const Card: React.FC<CardData> = ({
           <motion.img
             src={backImgSrc}
             alt={backText}
-            className="mb-2 h-40 w-full object-cover"
+            className="mb-2 h-64 w-full rounded-lg object-cover"
           />
-          <span className="text-lg">{backText}</span>
+          <motion.div className="z-50 px-3 py-10">
+            <h2 className="text-2xl font-bold text-white">{backTitle}</h2>
+            <hr className="mx-auto my-2 h-[2px] w-2/6 border-0 bg-secondary" />
+            <h5 className="h-64 overflow-hidden text-base text-white">
+              {backText}
+            </h5>
+            <hr className="mx-auto my-2 h-[2px] w-2/6 border-0 bg-secondary" />
+          </motion.div>
+          <motion.div className="absolute bottom-0 right-0">
+            <motion.img
+              src="./img/cards/svg/back.svg"
+              alt={frontTitle}
+              className="z-0 h-44 w-full object-cover"
+            />
+          </motion.div>
         </motion.div>
       </div>
     </>
@@ -116,6 +141,7 @@ const CardGrid: React.FC = () => {
           frontTitle={card.frontTitle}
           frontText={card.frontText}
           backImgSrc={card.backImgSrc}
+          backTitle={card.backTitle}
           backText={card.backText}
         />
       ))}
