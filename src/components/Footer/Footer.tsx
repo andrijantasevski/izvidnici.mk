261db7e1abd423fb6baa743421558c284b02f370
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-06-10 12:58:33 +0200

    resolved issue with overflow on mobile

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -22,44 +22,42 @@ const footerLinks: FooterLink[] = [
 
 const Footer = () => {
   return (
-    <footer className="footer-container">
-      <div className=" flex h-full w-full items-center justify-center">
-        <div className="box">
-          <div className="m-5 flex items-center justify-center">
-            <Image src={footerIcon} alt="Banner Image" />
-          </div>
-          <div className="navigation-links my-10">
-            <ul className="flex justify-center">
-              {footerLinks.map((link, index) => (
-                <li key={index} className="mx-4 my-1">
-                  <Link href={link.linkTo}>
-                    <p className="text-slate-100 hover:text-slate-50">
-                      {link.siteName}
-                    </p>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="mt-4 flex justify-between">
-            <div className="flex items-center">
-              <BsTiktok className="mr-5 text-2xl text-slate-100" />
-              <BsFacebook className="mr-5 text-2xl text-slate-100" />
-              <BsInstagram className="text-2xl text-slate-100" />
-            </div>
-            <ul className="flex items-center">
-              <li className="mr-6">
-                <a href="mailto: sim@scout.org.mk" className="text-slate-100">
-                  sim@scout.org.mk
-                </a>
+    <footer className="footer-container container">
+      <div className="box">
+        <div className="m-5 flex items-center justify-center">
+          <Image src={footerIcon} alt="Banner Image" />
+        </div>
+        <div className="navigation-links my-10">
+          <ul className="flex flex-wrap justify-center">
+            {footerLinks.map((link, index) => (
+              <li key={index} className="mx-2 my-1">
+                <Link href={link.linkTo}>
+                  <p className="text-slate-100 hover:text-slate-50">
+                    {link.siteName}
+                  </p>
+                </Link>
               </li>
-              <li>
-                <a href="tel:+02 311 2254" className="text-slate-100">
-                  02 311 2254
-                </a>
-              </li>
-            </ul>
+            ))}
+          </ul>
+        </div>
+        <div className="mt-4 flex justify-between">
+          <div className="flex items-center">
+            <BsTiktok className="mr-4 text-2xl text-slate-100" />
+            <BsFacebook className="mr-4 text-2xl text-slate-100" />
+            <BsInstagram className="text-2xl text-slate-100" />
           </div>
+          <ul className="flex items-center">
+            <li className="mr-4">
+              <a href="mailto:sim@scout.org.mk" className="text-slate-100">
+                sim@scout.org.mk
+              </a>
+            </li>
+            <li>
+              <a href="tel:+023112254" className="text-slate-100">
+                02 311 2254
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
