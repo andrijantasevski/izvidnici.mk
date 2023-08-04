8c74caef9556a8aebf64e9eca512e0ec6aaf81f9
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-08-04 20:47:12 +0200

    wip: refactor sections on obuka page and converted to a single component.

--- src/components/obuka-page/ObukaComponent.tsx
+++ src/components/obuka-page/ObukaComponent.tsx
@@ -1,102 +1,120 @@
 import React from "react";
 import Image from "next/image";
+import { TrainingSchema } from "../../../data/training";
 
-interface ObukaData {
-  whatisLearn: string;
-  learnText: string;
-  ForHoItis: string;
-  forHoText: string;
-  Svg: string;
-  SvgText: string;
-  HowisWon: string;
-  HowisWonText: string;
-}
+type Training = {
+  /** A background color for the section. It has to be in the format of: bg-[#HEX] */
+  backgroundColor: `bg-[${string}]`;
 
-const data: ObukaData[] = [
-  {
-    whatisLearn: "ШТО СЕ ИЗУЧУВА",
-    learnText:
-      "Прогресивната патека за личен развој во извидничкото движење започнува токму со оваа школа. Истата опфаќа теоретска едукација и развој на лидерски вештини кај секој учесник, основни меки вештини, имплементирање на веќе постоечки програми за деца и млади од основната програма на СИМ и вештини за промоција на извидништвото и извидничките вредности. Накратко, се имплементира курикулум создаден од комбинација на меки вештини, лидерски вештини и основни извиднички вештини.",
-    ForHoItis: "ЗА КОГО Е НАМЕНЕТА",
-    forHoText:
-      "Регистрирани членови во извидничката организација, на возраст над 14 години.",
-    Svg: "/img/obuka-page/poligon.svg",
-    SvgText: "ШКОЛА ЗА ВОДАЧИ 1",
-    HowisWon: "КАКО СЕ ОСВОЈУВА",
-    HowisWonText:
-      "Со активно учество на ШЗВ 1; Со планирање и имплементација на сесија / работилница во рамките на Летен / Зимски Камп, Ден на извидници, Извиднички караван.",
-  },
-];
+  /** What is learnt with the training. */
+  whatWeLearn: string;
 
-const ObukaComponent: React.FC = () => (
-  <div className="relative bg-[#455227]">
-    <div className="absolute bottom-0 right-0 hidden md:block">
-      <Image
-        src="./img/cards/svg/back-card.svg"
-        width={474}
-        height={490}
-        className="z-0 w-full object-cover"
-        alt="image svg"
-      />
-    </div>
-    <div className="absolute bottom-0 left-0 hidden md:block">
-      <Image
-        src="./img/cards/svg/svg-cards.svg"
-        width={474}
-        height={490}
-        className="z-0 w-full object-cover"
-        alt="image svg"
-      />
-    </div>
+  /** For whom is the training intended. */
+  forWhom: { id: number; description: string }[];
 
-    {data.map((item) => (
-      <div key={item.whatisLearn} className="py-10">
-        <div className=" relative mx-auto w-10/12 max-w-screen-xl rounded-3xl border border-base-100 px-7 py-10 text-white ">
+  /** How can the skills be acquired. */
+  howItIsAcquired: { id: number; description: string }[];
+
+  /** Type of training, e.g. Обука за обучувачи. */
+  typeOfTraining: string;
+
+  /** Image for the type of training. It should be provided as svg. */
+  typeOfTrainingImageSrc: `${string}.svg`;
+};
+
+export type TrainingComponentProps = {
+  training: TrainingSchema;
+};
+
+export default function TrainingComponent({
+  training,
+}: TrainingComponentProps) {
+  return (
+    <section className={`relative ${training.backgroundColor}`}>
+      <div className="absolute bottom-0 right-0 hidden md:block">
+        <Image
+          src="./img/cards/svg/back-card.svg"
+          width={474}
+          height={490}
+          className="z-0 w-full object-cover"
+          // TODO
+          // IMPROVE ALT DESC
+          alt="image svg"
+        />
+      </div>
+
+      <div className="absolute bottom-0 left-0 hidden md:block">
+        <Image
+          src="./img/cards/svg/svg-cards.svg"
+          width={474}
+          height={490}
+          className="z-0 w-full object-cover"
+          // TODO
+          // IMPROVE ALT DESC
+          alt="image svg"
+        />
+      </div>
+
+      <div className="py-10">
+        <div className=" relative mx-auto w-10/12 max-w-screen-xl rounded-3xl border border-base-100 px-7 py-10 text-white">
           <div className="ml-7">
-            <h2 className="mb-4 text-xl font-semibold">{item.whatisLearn}</h2>
+            <h2 className="mb-4 text-xl font-semibold uppercase">
+              што се изучува
+            </h2>
           </div>
+
           <div className="flex gap-5">
             <div className=" mt-2 h-2 w-2 rounded-full bg-white md:block"></div>
             <div className=" w-11/12">
-              <p>{item.learnText}</p>
+              <p>{training.whatWeLearn}</p>
             </div>
           </div>
         </div>
         <div className="mx-auto grid w-10/12 max-w-screen-xl gap-10 py-10 md:grid-cols-3 md:gap-40">
           <div className="relative z-40 mx-auto flex w-11/12  flex-col justify-center space-y-10 rounded-3xl border border-base-100 px-7 py-4 text-white ">
             <div className="ml-4">
-              <h2 className="mb-4  text-xl font-semibold">
-                {item.ForHoItis}
+              <h2 className="mb-4 text-xl font-semibold uppercase">
+                за кого е наменета
               </h2>
             </div>
+
             <div className="flex gap-5">
-              <div className=" mt-2 h-2 w-2 rounded-full bg-white md:block"></div>
-              <div className=" w-11/12">
-                <p>{item.forHoText}</p>
+              <div className="mt-2 h-2 w-2 rounded-full bg-white md:block"></div>
+              <div className="w-11/12">
+                {training.forWhom.map((trainingForWhom) => (
+                  <p key={trainingForWhom.id}>{trainingForWhom.description}</p>
+                ))}
               </div>
             </div>
           </div>
           <div className="z-40 flex flex-col items-center justify-between  rounded-3xl px-4 py-4 text-white">
-            <Image src={item.Svg} alt="SVG Icon" width={348} height={320} />
-            <p className="mt-5 text-center text-xl font-semibold">
-              {item.SvgText}
+            <Image
+              src={training.typeOfTrainingImageSrc}
+              alt="SVG Icon"
+              width={348}
+              height={320}
+            />
+            <p className="mt-5 text-center text-xl font-semibold uppercase">
+              {training.typeOfTraining}
             </p>
           </div>
           <div className=" relative mx-auto flex w-11/12 flex-col justify-center space-y-10 rounded-3xl border border-base-100 px-7 py-4 text-white">
             <div className="ml-4">
-              <h2 className="mb-4 text-xl font-semibold">{item.HowisWon}</h2>
+              <h2 className="mb-4 text-xl font-semibold uppercase">
+                како се освојува
+              </h2>
             </div>
-            <div className="flex gap-5">
-              <div className=" mt-2 h-2 w-2 rounded-full bg-white md:block"></div>
-              <div className=" w-11/12">
-                <p>{item.HowisWonText}</p>
-              </div>
-            </div>
+
+            <ul className="flex flex-col gap-5">
+              {training.howItIsAcquired.map((howItIsAcquiredSingle) => (
+                <li key={howItIsAcquiredSingle.id}>
+                  {howItIsAcquiredSingle.description}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-    ))}
-  </div>
-);
-
-export default ObukaComponent;
+    </section>
+  );
+}
--- src/pages/obuka.tsx
+++ src/pages/obuka.tsx
@@ -1,12 +1,11 @@
 import HeroBanner from "@/components/common/HeroBanner";
 import HeroDescription from "@/components/common/HeroDescription";
-import ObukaComponent from "@/components/obuka-page/ObukaComponent";
-import ObukaFour from "@/components/obuka-page/ObukaFour";
-import ObukaSecond from "@/components/obuka-page/ObukaSecond";
-import ObukaThree from "@/components/obuka-page/ObukaThree";
 import SectionBetween from "@/components/obuka-page/SectionBetween";
 import Head from "next/head";
 import Image from "next/image";
+import trainingDataParsed from "../../data/training";
+import TrainingComponent from "@/components/obuka-page/ObukaComponent";
+import { Fragment } from "react";
 
 export default function obuka() {
   return (
@@ -28,18 +27,19 @@ export default function obuka() {
           description="Покрај областите на личен развој предвидени за категориите развигорци и извидници, за нашите истражувачи, ровери и сениори има креирано прогресивна тренинг рамка која одговара на потребите и стремежите на младите од денешницата. Сојуз на извидници на Македонија продолжува да се залага за креирање на конструктивни индивидуи кои позитивно влијаат и придонесуваат во заедницата во која делуваат."
         />
 
-        <ObukaComponent />
-        <SectionBetween />
-        <ObukaSecond />
-        <SectionBetween />
-        <ObukaThree />
-        <SectionBetween />
-        <ObukaFour />
+        {trainingDataParsed.map((training, idx) => (
+          <Fragment key={training.id}>
+            <TrainingComponent training={training} />
+
+            {idx + 1 < trainingDataParsed.length && <SectionBetween />}
+          </Fragment>
+        ))}
 
         <div className=" py-10">
           <h4 className=" py-14 text-center text-xl font-bold md:text-2xl">
             Работен век на еден извидник
           </h4>
+
           <div className=" mx-auto w-10/12 max-w-screen-xl">
             <Image
               src="./img/obuka-page/obuka-last.svg"
