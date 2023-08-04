895825163fbb6d6ba51ceb20efa69e699c7bd7aa
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-08-04 20:51:27 +0200

    wip: fix events page.

--- src/pages/_nastani/[eventId].tsx
+++ src/pages/_nastani/[eventId].tsx
@@ -11,108 +11,112 @@ type NastanPageProps = {
   event: EventType;
 };
 
-const NastanPage: NextPage<NastanPageProps> = ({ event }) => {
-  return (
-    <main className="bg-primary">
-      <section className="mx-auto flex w-11/12 max-w-screen-xl flex-col gap-10 py-40 text-base-100">
-        <div className="flex w-full items-center justify-center">
-          <Image
-            src={`${env.NEXT_PUBLIC_API_BASE_URL}/${event.picture}`}
-            width={800}
-            height={600}
-            alt={event.title}
-            priority
-            className="rounded-xl border-2 border-secondary"
-          />
-        </div>
+export default function NastanPage() {
+  return <div>Event</div>;
+}
 
-        <div className="flex flex-col items-center justify-center gap-6">
-          <h1 className="text-3xl font-bold">{event.title}</h1>
+// const NastanPage: NextPage<NastanPageProps> = ({ event }) => {
+//   return (
+//     <main className="bg-primary">
+//       <section className="mx-auto flex w-11/12 max-w-screen-xl flex-col gap-10 py-40 text-base-100">
+//         <div className="flex w-full items-center justify-center">
+//           <Image
+//             src={`${env.NEXT_PUBLIC_API_BASE_URL}/${event.picture}`}
+//             width={800}
+//             height={600}
+//             alt={event.title}
+//             priority
+//             className="rounded-xl border-2 border-secondary"
+//           />
+//         </div>
 
-          <div className="flex w-full items-center justify-center gap-6">
-            <div className="flex items-center gap-1">
-              <IconCalendar className="text-secondary" />
-              {format(new Date(event.start_date), "dd/MM/y")}
-              {" - "}
-              {format(new Date(event.end_date), "dd/MM/y")}
-            </div>
+//         <div className="flex flex-col items-center justify-center gap-6">
+//           <h1 className="text-3xl font-bold">{event.title}</h1>
 
-            <div className="flex items-center gap-1">
-              <IconMapPin className="text-secondary" />
-              {event.place}
-            </div>
-          </div>
-        </div>
+//           <div className="flex w-full items-center justify-center gap-6">
+//             <div className="flex items-center gap-1">
+//               <IconCalendar className="text-secondary" />
+//               {format(new Date(event.start_date), "dd/MM/y")}
+//               {" - "}
+//               {format(new Date(event.end_date), "dd/MM/y")}
+//             </div>
 
-        <p>{event.desc}</p>
+//             <div className="flex items-center gap-1">
+//               <IconMapPin className="text-secondary" />
+//               {event.place}
+//             </div>
+//           </div>
+//         </div>
 
-        <p className="leading-loose">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-          cumque perspiciatis accusamus id soluta quo alias numquam ipsum in!
-          Sunt molestias asperiores laborum commodi et natus non, placeat harum
-          laboriosam, sapiente dolore repudiandae omnis voluptatem quisquam ut
-          explicabo eligendi quis! Rem, sed consequuntur! Veniam magnam quas
-          omnis qui consequuntur iusto necessitatibus laboriosam saepe, tempora
-          molestias in dicta perspiciatis numquam minus. Explicabo vel
-          reiciendis quo, quibusdam et in aperiam sit enim. Error dolores
-          quibusdam aut delectus sequi odit! Animi, expedita! Eius dolore quasi
-          ipsum cum! Sunt quibusdam non, molestiae cum debitis magni reiciendis
-          soluta beatae. Deserunt ut quidem molestiae tempora a natus veniam
-          harum accusantium, quas repellat esse aliquid voluptas repudiandae ab
-          laboriosam ex voluptatum dolore error sint aperiam. Eum, ducimus
-          molestias laboriosam aperiam dolorem reprehenderit, quaerat corporis
-          reiciendis qui molestiae odit maiores illum voluptas saepe
-          consequuntur libero adipisci eius at. Necessitatibus ex explicabo
-          cupiditate ducimus accusantium sed a aperiam excepturi!
-        </p>
+//         <p>{event.desc}</p>
 
-        <Button asChild variant="secondary" size="lg">
-          <Link href="/zacleni-se">Доаѓам</Link>
-        </Button>
-      </section>
-    </main>
-  );
-};
+//         <p className="leading-loose">
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+//           cumque perspiciatis accusamus id soluta quo alias numquam ipsum in!
+//           Sunt molestias asperiores laborum commodi et natus non, placeat harum
+//           laboriosam, sapiente dolore repudiandae omnis voluptatem quisquam ut
+//           explicabo eligendi quis! Rem, sed consequuntur! Veniam magnam quas
+//           omnis qui consequuntur iusto necessitatibus laboriosam saepe, tempora
+//           molestias in dicta perspiciatis numquam minus. Explicabo vel
+//           reiciendis quo, quibusdam et in aperiam sit enim. Error dolores
+//           quibusdam aut delectus sequi odit! Animi, expedita! Eius dolore quasi
+//           ipsum cum! Sunt quibusdam non, molestiae cum debitis magni reiciendis
+//           soluta beatae. Deserunt ut quidem molestiae tempora a natus veniam
+//           harum accusantium, quas repellat esse aliquid voluptas repudiandae ab
+//           laboriosam ex voluptatum dolore error sint aperiam. Eum, ducimus
+//           molestias laboriosam aperiam dolorem reprehenderit, quaerat corporis
+//           reiciendis qui molestiae odit maiores illum voluptas saepe
+//           consequuntur libero adipisci eius at. Necessitatibus ex explicabo
+//           cupiditate ducimus accusantium sed a aperiam excepturi!
+//         </p>
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await fetch(`${env.API_BASE_URL}/api/events`);
+//         <Button asChild variant="secondary" size="lg">
+//           <Link href="/zacleni-se">Доаѓам</Link>
+//         </Button>
+//       </section>
+//     </main>
+//   );
+// };
 
-  const allEvents: { events: EventType[] } = await response.json();
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const response = await fetch(`${env.API_BASE_URL}/api/events`);
 
-  const staticPaths = allEvents.events.map((event) => ({
-    params: { eventId: String(event.id) },
-  }));
+//   const allEvents: { events: EventType[] } = await response.json();
 
-  return {
-    paths: staticPaths,
-    fallback: false,
-  };
-};
+//   const staticPaths = allEvents.events.map((event) => ({
+//     params: { eventId: String(event.id) },
+//   }));
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  if (params) {
-    const response = await fetch(
-      `${env.API_BASE_URL}/api/events/${params.eventId}`
-    );
+//   return {
+//     paths: staticPaths,
+//     fallback: false,
+//   };
+// };
 
-    const event: { event: EventType } = await response.json();
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   if (params) {
+//     const response = await fetch(
+//       `${env.API_BASE_URL}/api/events/${params.eventId}`
+//     );
 
-    if (!event) {
-      return {
-        props: {},
-      };
-    }
+//     const event: { event: EventType } = await response.json();
 
-    return {
-      props: {
-        event: event.event,
-      },
-    };
-  }
+//     if (!event) {
+//       return {
+//         props: {},
+//       };
+//     }
 
-  return {
-    props: {},
-  };
-};
+//     return {
+//       props: {
+//         event: event.event,
+//       },
+//     };
+//   }
 
-export default NastanPage;
+//   return {
+//     props: {},
+//   };
+// };
+
+// export default NastanPage;
