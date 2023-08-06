a6d01a032d49fc6993abc125dfeb1d4025e6fd20
Panter530xd <bobansenic@gmail.com>
2023-08-06 13:22:33 +0200

    making the link to work in slider image to be as him parent

--- src/components/programmes/ProgrammesSlider.tsx
+++ src/components/programmes/ProgrammesSlider.tsx
@@ -7,7 +7,7 @@ export default function ProgrammesSlider() {
   const splideOptions = {
     type: "loop",
     perPage: 4,
-    gap: "2rem",
+    gap: "1rem",
     breakpoints: {
       768: {
         perPage: 1,
@@ -16,22 +16,27 @@ export default function ProgrammesSlider() {
     pagination: false,
   };
 
-  const handleImageClick = (pdfUrl: string) => {
-    window.open(pdfUrl, "_blank");
-  };
-
   return (
     <section className="bg-primary bg-programi-up-right py-10">
       <div className="container mx-auto">
         <Splide options={splideOptions}>
           {programmesPDFsParsed.map((programme) => (
-            <SplideSlide key={programme.id}>
-              <img
-                src={programme.pdfImageSrc}
-                alt={`Programme ${programme.id}`}
-                className="mx-auto h-auto w-56 cursor-pointer rounded-md shadow-lg transition-shadow hover:shadow-xl"
-                onClick={() => handleImageClick(programme.pdfUrl)}
-              />
+            <SplideSlide
+              key={programme.id}
+              className="md:px-12 lg:px-14 xl:px-16"
+            >
+              <a
+                href={programme.pdfUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={programme.pdfImageSrc}
+                  alt={`Programme ${programme.id}`}
+                  //TODO check the sizes of images on mobile
+                  className="mx-auto  h-auto w-64 cursor-pointer rounded-md object-cover shadow-lg transition-shadow hover:shadow-xl md:h-full md:w-full"
+                />
+              </a>
             </SplideSlide>
           ))}
         </Splide>
